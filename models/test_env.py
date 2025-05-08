1becc2ef7979fcefad34b9eafda3971302d47d3b
xodls <pider6@naver.com>
2025-05-08 15:54:07 +0900

    첫 커밋, 기본환경설정

---
+++ models/test_env.py
@@ -0,0 +1,7 @@
+import torch
+import torchmetrics
+import pytorch_lightning as pl
+
+print("torch:", torch.__version__)
+print("torchmetrics:", torchmetrics.__version__)
+print("lightning:", pl.__version__)

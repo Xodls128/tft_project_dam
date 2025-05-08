bb25e19474ea29014bf36c9f82c8344068888466
xodls <pider6@naver.com>
2025-05-08 17:35:46 +0900

    FEATURE: tft_trainer.py

--- models/tft_trainer.py
+++ models/tft_trainer.py
@@ -1,1 +1,18 @@
 # models/tft_trainer.py
+
+from pytorch_lightning import seed_everything
+from data_module import TFTDataModule
+from tft_trainer import TFTTrainer
+
+if __name__ == "__main__":
+    seed_everything(42)
+
+    data_module = TFTDataModule(path="../data/sample_sales_202408_202504.xlsx")
+    data_module.load_and_preprocess()
+    data_module.setup()
+    dataloader = data_module.get_dataloader()
+
+    trainer = TFTTrainer(dataset=data_module.dataset)
+    trainer.build_model()
+    trainer.train(dataloader)
+    trainer.save("../models/tft_model.ckpt")

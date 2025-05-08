01e524510cf8ded5a53c396516f7db17563d27a2
xodls <pider6@naver.com>
2025-05-08 17:40:23 +0900

    FEATURE: tft_trainer.py 수정

--- models/tft_trainer.py
+++ models/tft_trainer.py
@@ -1,18 +1,33 @@
 # models/tft_trainer.py
 
-from pytorch_lightning import seed_everything
-from data_module import TFTDataModule
-from tft_trainer import TFTTrainer
+import torch
+from pytorch_forecasting import TemporalFusionTransformer
+from pytorch_lightning import Trainer
+from pytorch_forecasting import TimeSeriesDataSet
+from torch.utils.data import DataLoader
 
-if __name__ == "__main__":
-    seed_everything(42)
+class TFTTrainer:
+    def __init__(self, dataset: TimeSeriesDataSet):
+        self.dataset = dataset
+        self.model = None
+        self.trainer = None
 
-    data_module = TFTDataModule(path="../data/sample_sales_202408_202504.xlsx")
-    data_module.load_and_preprocess()
-    data_module.setup()
-    dataloader = data_module.get_dataloader()
+    def build_model(self):
+        self.model = TemporalFusionTransformer.from_dataset(
+            self.dataset,
+            learning_rate=0.03,
+            hidden_size=16,
+            attention_head_size=1,
+            dropout=0.1,
+            loss=torch.nn.L1Loss(),
+            log_interval=10,
+            reduce_on_plateau_patience=4,
+        )
 
-    trainer = TFTTrainer(dataset=data_module.dataset)
-    trainer.build_model()
-    trainer.train(dataloader)
-    trainer.save("../models/tft_model.ckpt")
+    def train(self, dataloader: DataLoader, max_epochs: int = 5):
+        self.trainer = Trainer(max_epochs=max_epochs, gradient_clip_val=0.1)
+        self.trainer.fit(self.model, train_dataloaders=dataloader)
+
+    def save(self, path: str):
+        self.trainer.save_checkpoint(path)
+        print(f"Model saved to {path}")

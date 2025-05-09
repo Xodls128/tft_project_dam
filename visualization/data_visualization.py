3a0459f3622e2df6d8be6895e1f8b42672f25be5
xodls <pider6@naver.com>
2025-05-09 10:10:45 +0900

    FEATURE: 다음주 예측량 시각화 한글깨짐>>폰트해결

--- visualization/data_visualization.py
+++ visualization/data_visualization.py
@@ -7,7 +7,12 @@ import matplotlib.font_manager as fm
 import seaborn as sns
 
 
+
 def plot_next_week_prediction():
+    # 한글 폰트 설정
+    plt.rcParams["font.family"] = "Malgun Gothic"
+    plt.rcParams["axes.unicode_minus"] = False
+
     df = pd.read_csv("./results/next_week_only.csv")
     df["date"] = pd.to_datetime(df["date"])
 
